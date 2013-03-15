b422ed151e89b3769f1dfb6e32fcce5ef34ad1c2
Alexandre Grigorovitch <alexezh@gmail.com>
2013-03-14 19:27:58 -0700

    initial checkin of JsonReader / JsonWriter

---
+++ jsonreader.h
@@ -0,0 +1,362 @@
+// Copyright (c) 2013 Alexandre Grigorovitch (alexezh@gmail.com).
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+#pragma once
+
+#include <string>
+#include <stack>
+#include "jsonstr.h"
+
+class JsonReader
+{
+public:
+	enum class NodeType
+	{
+		None,
+		String,
+		Int,
+		Array,
+		EndArray,
+		Object,
+		EndObject,
+	};
+
+public:
+	JsonReader(const char * pszBlob, size_t cchBlob)
+	{
+		InitState(pszBlob, cchBlob);
+	}
+
+	JsonReader(const std::vector<char> & blob)
+	{
+		InitState(blob.data(), blob.size());
+	}
+
+	bool ReadNext()
+	{
+		char c;
+		bool ret = false;
+
+		_Name.resize(0);
+		_Value.resize(0);
+
+        // simple state machine for parsing
+        //  "name" : val
+        //  "name" : "val"
+		for(;;)
+		{
+			c = ReadChar();
+			if(c == '\0') 
+			{
+				break;
+			}
+			if(_State == ParseState::StartReadName)
+			{
+				if(IsWhite(c))
+				{
+					;
+				}
+				else if(c == '"')
+				{
+					_State = ParseState::ReadName;
+				}
+				else 
+				{
+					assert(false);
+					break;
+				}
+			}
+			else if(_State == ParseState::ReadName)
+			{
+				// for now ignore escaping
+				if(c != '"')
+				{
+					_Name.push_back(c);
+				}
+				else
+				{
+					_State = ParseState::ReadColon;
+				}
+			}
+			else if(_State == ParseState::ReadColon)
+			{
+				if(IsWhite(c))
+				{
+					;
+				}
+				else if(c == ':')
+				{
+					_State = ParseState::StartReadValue;
+				}
+				else 
+				{
+					assert(false);
+					break;
+				}
+			}
+			else if(_State == ParseState::StartReadValue)
+			{
+				if(IsWhite(c))
+				{
+					;
+				}
+				else if(c == '"')
+				{
+					_Type = NodeType::String;
+					_State = ParseState::ReadStringValue;
+				}
+				else if(c == '[')
+				{
+					_Type = NodeType::Array;
+
+					// start next iteration from reading values
+					_State = ParseState::StartReadValue;
+					_ParseStack.push(ParseNode(_Name, NodeType::Array));
+					ret = true;
+					break;
+				}
+				else if(c == '{')
+				{
+					_Type = NodeType::Object;
+
+					// start next iteration from reading name
+					_State = ParseState::StartReadName;
+					_ParseStack.push(ParseNode(_Name, NodeType::Object));
+					ret = true;
+					break;
+				}
+				else if(c >= '0' && c <= '9')
+				{
+					_Type = NodeType::Int;
+					_State = ParseState::ReadIntValue;
+
+                    _Value.push_back(c);
+                    if(!(PeekChar() >= '0' && PeekChar() <= '9'))
+                    {
+                        _IntValue = atoi(&_Value[0]);
+                        _State = ParseState::EndReadValue;
+						ret = true;
+						break;
+                    }
+				}
+				else
+				{
+					assert(false);
+					break;
+				}
+			}
+			else if(_State == ParseState::ReadStringValue)
+			{
+				// for now ignore escaping
+                if(c == '\\')
+                {
+                    c = ReadUnescape();
+					_Value.push_back(c);
+                }
+				else if(c != '"')
+				{
+					_Value.push_back(c);
+				}
+				else
+				{
+					_State = ParseState::EndReadValue;
+					ret = true;
+					break;
+				}
+			}
+			else if(_State == ParseState::ReadIntValue)
+			{
+                _Value.push_back(c);
+                if(!(PeekChar() >= '0' && PeekChar() <= '9'))
+                {
+                    _IntValue = atoi(&_Value[0]);
+                    _State = ParseState::EndReadValue;
+					ret = true;
+					break;
+                }
+			}
+			else if(_State == ParseState::EndReadValue)
+			{
+				if(IsWhite(c))
+				{
+					continue;
+				}
+				else if(c == ',')
+				{
+					// if we inside array, keep reading values
+					// otherwise read name
+					if(_ParseStack.size() > 0 && _ParseStack.top().Type == NodeType::Array)
+					{
+						_State = ParseState::StartReadValue;
+					}
+					else
+					{
+						_State = ParseState::StartReadName;
+					}
+
+					continue;
+				}
+				else if(c == '}')
+				{
+					// keep parse state as EndReadValue
+					// on next iteration we might get another closure 
+					_State = ParseState::EndReadValue;
+					_Type = NodeType::EndObject;
+					_Name = _ParseStack.top().Name;
+					_ParseStack.pop();
+					ret = true;
+					break;
+				}
+				else if(c == ']')
+				{
+					// keep parse state as EndReadValue
+					// on next iteration we might get another closure 
+					_State = ParseState::EndReadValue;
+					_Type = NodeType::EndArray;
+					_Name = _ParseStack.top().Name;
+					_ParseStack.pop();
+					ret = true;
+					break;
+				}
+				else
+				{
+					assert(false);
+				}
+			}
+		}
+
+		return ret;
+	}
+
+	NodeType Type() { return _Type; }
+    bool IsString() { return _Type == NodeType::String; }
+	const std::string & Name() { return _Name; }
+	const std::string & Value() { return _Value; }
+	int IntValue() { return _IntValue; }
+
+private:
+	bool IsWhite(char c)
+	{
+		return (c == ' ' || c == '\n');
+	}
+
+    /*
+        Following chars are escaped
+        \"
+        \\
+        \/
+        \b
+        \f
+        \n
+        \r
+        \t
+        \u four-hex-digits
+    */
+
+    char ReadChar()
+	{
+		if(_pszCur < _pszEnd)
+		{
+			char c = *_pszCur;
+			_pszCur++;
+			return c;
+		}
+		else
+		{
+			return '\0';
+		}
+    }
+    char ReadUnescape()
+	{
+        char c = ReadChar();
+        switch(c)
+        {
+            case '"': 
+            case '\\': return c;
+            case 'b': return '\b';
+            case 'f': return '\f';
+            case 'n': return '\n';
+            case 'r': return '\r';
+            case 't': return '\t';
+            default:
+			    assert(false);
+                return '\0';
+        }
+	}
+	char PeekChar()
+	{
+		if(_pszCur+1 < _pszEnd)
+		{
+			char c = *(_pszCur+1);
+			return c;
+		}
+		else
+		{
+			return '\0';
+		}
+	}
+	void InitState(const char * pszBlob, size_t cchBlob)
+	{
+		_pszBegin = pszBlob;
+		_pszEnd = pszBlob + cchBlob;
+		_pszCur = _pszBegin;
+
+		_State = ParseState::StartReadName;
+		_Name.resize(0);
+		_Value.resize(0);
+	}
+
+	enum class ParseState
+	{
+		StartReadName,
+		ReadName,
+		ReadColon,
+		StartReadValue,
+		ReadStringValue,
+		ReadIntValue,
+		EndReadValue,
+		StartReadObject,
+		StartReadArray,
+	};
+	ParseState _State;
+
+	struct ParseNode
+	{
+		ParseNode(const std::string & name, NodeType type)
+			: Name(name)
+			, Type(type)
+		{
+		}
+
+		std::string Name;
+		NodeType Type;
+	};
+
+	// stack of nodes 
+	std::stack<ParseNode> _ParseStack;
+	const char * _pszBegin;
+	const char * _pszCur;
+	const char * _pszEnd;
+
+    NodeType _Type;
+	std::string _Name;
+	std::string _Value;
+    int _IntValue;
+};
---
+++ jsonwriter.h
@@ -0,0 +1,172 @@
+// Copyright (c) 2013 Alexandre Grigorovitch (alexezh@gmail.com).
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+#pragma once
+
+#include <string>
+#include <vector>
+#include "jsonstr.h"
+
+class JsonWriter
+{
+public:
+	JsonWriter()
+		: _AddSep(false)
+	{
+	}
+	
+	JsonWriter & WriteName(const char * pszName, size_t cchName)
+	{
+		WriteNameWorker(pszName, cchName);
+		return *this;
+	}
+	JsonWriter & WriteName(const std::string & name)
+	{
+		WriteNameWorker(name.c_str(), name.length());
+		return *this;
+	}
+
+	JsonWriter & WriteStringPair(const char * pszName, size_t cchName, const char * pszValue, size_t cchValue)
+	{
+		WriteNameWorker(pszName, cchName);
+		cchValue = (cchValue > 0) ? cchValue : strlen(pszValue);
+		WriteString(pszValue, cchValue);
+		_AddSep = true;
+		return *this;
+	}
+	JsonWriter & WriteStringPair(const char * pszName, size_t cchName, const std::string & value)
+	{
+		WriteNameWorker(pszName, cchName);
+		WriteString(value.c_str(), value.length());
+		_AddSep = true;
+		return *this;
+	}
+	JsonWriter & WriteStringValue(const char * pszValue, size_t cchValue)
+	{
+		cchValue = (cchValue > 0) ? cchValue : strlen(pszValue);
+		WriteString(pszValue, cchValue);
+		_AddSep = true;
+		return *this;
+	}
+	JsonWriter & WriteStringValue(const std::string & value)
+	{
+		WriteString(value.c_str(), value.length());
+		_AddSep = true;
+		return *this;
+	}
+
+	JsonWriter & WriteIntValue(int val)
+	{
+		// convert int to string and append
+		std::vector<char> arVal;
+		arVal.resize(32);
+		_itoa_s(val, &arVal[0], arVal.size(), 10);
+		arVal.resize(strlen(&arVal[0]));
+		_Result.insert(_Result.end(), arVal.begin(), arVal.end());
+		_AddSep = true;
+		return *this;
+	}
+
+	JsonWriter & WriteObjectValueStart()
+	{
+		if(_AddSep)
+		{
+			_Result.push_back(',');
+			_AddSep = false;
+		}
+
+		_Result.push_back('{');
+		_Result.push_back('\n');
+		return *this;
+	}
+	
+	JsonWriter & WriteObjectValueEnd()
+	{
+		_Result.push_back('\n');
+		_Result.push_back('}');
+		_Result.push_back('\n');
+		_AddSep = true;
+		return *this;
+	}
+
+	JsonWriter & WriteArrayValueStart()
+	{
+		if(_AddSep)
+		{
+			_Result.push_back(',');
+			_AddSep = false;
+		}
+
+		_Result.push_back('[');
+		_Result.push_back('\n');
+		return *this;
+	}
+	
+	JsonWriter & WriteArrayValueEnd()
+	{
+		_Result.push_back('\n');
+		_Result.push_back(']');
+		_Result.push_back('\n');
+		_AddSep = true;
+		return *this;
+	}
+
+	const std::vector<char> & Result() { return _Result; }
+
+private:
+	void WriteNameWorker(const char * pszName, size_t cchName)
+	{
+		if(_AddSep)
+		{
+			_Result.push_back(',');
+			_AddSep = false;
+		}
+
+		WriteString(pszName, cchName);
+		_Result.push_back(':');
+	}
+
+	void WriteString(const char * pszVal, long cchVal)
+	{
+		_Result.push_back('"');
+		for(;*pszVal;pszVal++)
+		{
+            char c = *pszVal;
+			// escape
+            switch(c)
+            {
+                case '"': 
+                case '\\': _Result.push_back('\\'); _Result.push_back(c); break;
+                case '\b': _Result.push_back('\\'); _Result.push_back('b'); break;
+                case '\f': _Result.push_back('\\'); _Result.push_back('f'); break;
+                case '\n': _Result.push_back('\\'); _Result.push_back('n'); break;
+                case '\r': _Result.push_back('\\'); _Result.push_back('r'); break;
+                case '\t': _Result.push_back('\\'); _Result.push_back('t'); break;
+                default:
+			        _Result.push_back(*pszVal);
+                    break;
+            }
+		}
+		_Result.push_back('"');
+	}
+private:
+	std::vector<char> _Result;
+	bool _AddSep;
+};
